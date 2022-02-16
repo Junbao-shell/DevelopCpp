fdee818bec172b6b403915a3174d859667be02d1
gaojunbao <junbao.gao@nanovision.com.cn>
2022-02-16 12:24:54 +0800

    add the linux so library useage file

---
+++ 3.Build/SharedLibrary/Module/src/add.cpp
@@ -0,0 +1,24 @@
+///////////////////////////////////////////////////////////
+/// @copyright copyright description
+///
+/// @brief add function for shared library
+///
+/// @file add.cpp
+///
+/// @author Jonathan
+///
+/// @date 2022-02-13
+///////////////////////////////////////////////////////////
+
+// Current Cpp header
+#include "add.h"
+// System header
+// C/C++ standard library header
+// External library header
+// Current module header
+// Root directory header
+
+int Add::add(int a, int b)
+{
+    return a + b;
+}
