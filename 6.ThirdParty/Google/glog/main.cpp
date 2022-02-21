a29773c95de7c963ee0262513adb27405d93635f
junbao-shell <junbaogao@foxmail.com>
2022-02-21 23:22:50 +0800

    add the glog dll and include file, compile complete but cannot run success

--- 6.ThirdParty/Google/glog/main.cpp
+++ 6.ThirdParty/Google/glog/main.cpp
@@ -1,14 +1,32 @@
-#include <glog/logging.h>
+///////////////////////////////////////////////////////////
+/// @copyright copyright description
+/// 
+/// @brief test glog demo
+/// 
+/// @file main.cpp
+/// 
+/// @author Jonathan
+/// 
+/// @date 2022-02-21
+///////////////////////////////////////////////////////////
 
-int main(int argc, char** argv) 
+// Current Cpp header
+// System header
+// C/C++ standard library header
+#include <iostream>
+// External library header
+#include "glog/logging.h"
+// Current module header
+// Root directory header
+
+int main(int argc, char **argv)
 {
-    google::InitGoogleLogging(argv[0]);
-
-    // FLAGS_logtostderr = true;
+    FLAGS_v = 4;
+    FLAGS_log_dir = "./log";
     FLAGS_alsologtostderr = true;
-    FLAGS_log_dir = "./";
-    FLAGS_v = 2;
-
+    FLAGS_colorlogtostderr = true;
+    google::InitGoogleLogging(argv[0]);
+    
     VLOG(0) << " i am 0";
     VLOG(1) << " i am 1";
     VLOG(2) << " i am 2";
